82a0597bc9436ed0a6ece4c9c79d7d0c58cc7d4b
shirlyLoveU <tianxuelihello@sina.cn>
2015-03-25 16:53:57 +0800

    add situ demo

--- staticTests/avalon.mobile.js
+++ staticTests/avalon.mobile.js
@@ -4902,6 +4902,7 @@ new function() {
             当触发hold和longtap事件时会触发touchcancel事件，从而阻止touchend事件的触发，继而保证在同时绑定tap和hold(longtap)事件时只触发其中一个事件
         */
         longTapTimeout = setTimeout(function() {
+            alert('longTap event fire')
             longTapTimeout = null
             fireEvent(element, "hold")
             fireEvent(element, "longtap")
@@ -4959,7 +4960,7 @@ new function() {
                 } else {
                     fastclick.focus(element)
                 }
-                // event.preventDefault()
+                event.preventDefault()
                 console.log("event.defaultPrevented : " +event.defaultPrevented)
                 fireEvent(element, 'tap')
                 avalon.fastclick.fireEvent(element, "click", event)
@@ -4985,45 +4986,12 @@ new function() {
     if (touchNames[3]) {
         document.addEventListener(touchNames[3], function(event) {
             console.log('document event type : '+event.type+'\n')
-
+            alert('cancel event')
             if (longTapTimeout) clearTimeout(longTapTimeout)
             if (touchTimeout) clearTimeout(touchTimeout)
             longTapTimeout = touchTimeout = null
             touchProxy = {}
         })
-    }
-    me["clickHook"] = function(data) {
-        function touchstart(event) {
-            console.log('element touchstart event element id: '+data.element.id+'\n')
-            console.log('element touchstart event target : '+event.target.id)
-            console.log('element event.fireByAvalon : '+event.fireByAvalon)
-            var $element = avalon(data.element)
-            $element.addClass(fastclick.activeClass)
-        }
-        function needFixClick(type) {
-            return type === "click"
-        }
-        // if (needFixClick(data.param) ? touchSupported : true) {
-            data.specialBind = function(element, callback) {
-                var _callback = callback
-                if (!element.bindStart) { // 如果元素上绑定了多个事件不做处理的话会绑定多个touchstart监听器，显然不需要
-                    element.bindStart = true
-                    element.addEventListener(touchNames[0], touchstart)
-                } 
-                callback = function(event) {
-                    console.log('callback event type : '+event.type + '\n')
-                    console.log('callback element id: '+element.id+'\n')
-                    avalon(element).removeClass(fastclick.activeClass)
-                    _callback.apply(this, arguments)
-                }
-                data.msCallback = callback
-                avalon.bind(element, data.param, callback)
-            }
-            data.specialUnbind = function() {
-                element.removeEventListener(touchNames[0], touchstart)
-                avalon.unbind(data.element, data.param, data.msCallback)
-            }
-        // }
     }
     //fastclick只要是处理移动端点击存在300ms延迟的问题
     //这是苹果乱搞异致的，他们想在小屏幕设备上通过快速点击两次，将放大了的网页缩放至原始比例。
--- staticTests/situDemo/avalon.mobile.js
+++ staticTests/situDemo/avalon.mobile.js
@@ -4892,6 +4892,7 @@ new function() {
         event.preventDefault()
     }
     function cancelLongTap() {
+        alert('cancelLongTap')
         if (longTapTimeout) clearTimeout(longTapTimeout)
         longTapTimeout = null
     }
@@ -4915,11 +4916,14 @@ new function() {
         /*
             当触发hold和longtap事件时会触发touchcancel事件，从而阻止touchend事件的触发，继而保证在同时绑定tap和hold(longtap)事件时只触发其中一个事件
         */
+        avalon(element).addClass(fastclick.activeClass)
         longTapTimeout = setTimeout(function() {
+            alert('longTap fired')
             longTapTimeout = null
             fireEvent(element, "hold")
             fireEvent(element, "longtap")
             touchProxy = {}
+            avalon(element).removeClass(fastclick.activeClass)
         }, fastclick.clickDuration)
         return true
     }
@@ -4953,6 +4957,7 @@ new function() {
             }
             fireEvent(element, "swipe", details)
             fireEvent(element, "swipe" + direction, details)
+            avalon(element).removeClass(fastclick.activeClass)
             touchProxy = {}
         } else {
             if (fastclick.canClick(element) && touchProxy.mx < fastclick.dragDistance && touchProxy.my < fastclick.dragDistance) {
@@ -4970,17 +4975,21 @@ new function() {
                 } else {
                     fastclick.focus(element)
                 }
+                event.preventDefault()
                 fireEvent(element, 'tap')
                 avalon.fastclick.fireEvent(element, "click", event)
+                avalon(element).removeClass(fastclick.activeClass)
                 if (touchProxy.isDoubleTap) {
                     fireEvent(element, "doubletap")
                     avalon.fastclick.fireEvent(element, "dblclick", event)
                     touchProxy = {}
+                    avalon(element).removeClass(fastclick.activeClass)
                 } else {
                     touchTimeout = setTimeout(function() {
                         clearTimeout(touchTimeout)
                         touchTimeout = null
                         touchProxy = {}
+                        avalon(element).removeClass(fastclick.activeClass)
                     }, 250)
                 }
             }
@@ -4993,40 +5002,41 @@ new function() {
     document.addEventListener(touchNames[2], touchend)
     if (touchNames[3]) {
         document.addEventListener(touchNames[3], function(event) {
+            alert('cancel event called')
             if (longTapTimeout) clearTimeout(longTapTimeout)
             if (touchTimeout) clearTimeout(touchTimeout)
             longTapTimeout = touchTimeout = null
             touchProxy = {}
         })
     }
-    me["clickHook"] = function(data) {
-        function touchstart(event) {
-            var $element = avalon(data.element)
-            $element.addClass(fastclick.activeClass)
-        }
-        function needFixClick(type) {
-            return type === "click"
-        }
-        if (needFixClick(data.param) ? touchSupported : true) {
-            data.specialBind = function(element, callback) {
-                var _callback = callback
-                if (!element.bindStart) { // 如果元素上绑定了多个事件不做处理的话会绑定多个touchstart监听器，显然不需要
-                    element.bindStart = true
-                    element.addEventListener(touchNames[0], touchstart)
-                } 
-                callback = function(event) {
-                    avalon(element).removeClass(fastclick.activeClass)
-                    _callback.apply(this, arguments)
-                }
-                data.msCallback = callback
-                avalon.bind(element, data.param, callback)
-            }
-            data.specialUnbind = function() {
-                element.removeEventListener(touchNames[0], touchstart)
-                avalon.unbind(data.element, data.param, data.msCallback)
-            }
-        }
-    }
+    // me["clickHook"] = function(data) {
+    //     function touchstart(event) {
+    //         var $element = avalon(data.element)
+    //         $element.addClass(fastclick.activeClass)
+    //     }
+    //     function needFixClick(type) {
+    //         return type === "click"
+    //     }
+    //     if (needFixClick(data.param) ? touchSupported : true) {
+    //         data.specialBind = function(element, callback) {
+    //             var _callback = callback
+    //             if (!element.bindStart) { // 如果元素上绑定了多个事件不做处理的话会绑定多个touchstart监听器，显然不需要
+    //                 element.bindStart = true
+    //                 element.addEventListener(touchNames[0], touchstart)
+    //             } 
+    //             callback = function(event) {
+    //                 avalon(element).removeClass(fastclick.activeClass)
+    //                 _callback.apply(this, arguments)
+    //             }
+    //             data.msCallback = callback
+    //             avalon.bind(element, data.param, callback)
+    //         }
+    //         data.specialUnbind = function() {
+    //             element.removeEventListener(touchNames[0], touchstart)
+    //             avalon.unbind(data.element, data.param, data.msCallback)
+    //         }
+    //     }
+    // }
     //fastclick只要是处理移动端点击存在300ms延迟的问题
     //这是苹果乱搞异致的，他们想在小屏幕设备上通过快速点击两次，将放大了的网页缩放至原始比例。
     var fastclick = avalon.fastclick = {
