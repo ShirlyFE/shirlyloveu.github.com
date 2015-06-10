f9f5d6ba6391ff2baadf399ab8a60750ae1bd02e
shirlyLoveU <tianxuelihello@sina.cn>
2015-06-10 19:04:59 +0800

    update note

--- shirlyDemo/node/cluster.js
+++ shirlyDemo/node/cluster.js
@@ -2,21 +2,28 @@ var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-    console.log("master start...");
+cluster.schedulingPolicy = cluster.SCHED_NONE;
+console.log('exec app at '+Date.now())
+console.log('cluster.isMaster : '+cluster.isMaster)
 
+if (cluster.isMaster) {
     // Fork workers.
     for (var i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
 
-    cluster.on('listening',function(worker,address){
-        console.log('listening: worker ' + worker.process.pid +', Address: '+address.address+":"+address.port);
-    });
-
     cluster.on('exit', function(worker, code, signal) {
         console.log('worker ' + worker.process.pid + ' died');
     });
 } else {
-    http.createServer().listen(4001); //端口设为0,nodejs会自动分配一个可用端口
+    var a = process.listeners('disconnect');
+    console.log('process.listeners("disconnect").length is : ' + a.length);
+    process.removeAllListeners('disconnect');
+    a = process.listeners('disconnect');
+    console.log('process.listeners("disconnect").length is : ' + a.length);
+
+    http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+    }).listen(4002);
 }
